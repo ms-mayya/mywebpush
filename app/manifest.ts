c79671ea5b204eb9faa0ecce47d4af99b0db6514
EchoLi <echoli@gouptech.com.tw>
2024-11-23 09:53:38 +0800

    Update manifest

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,8 +2,8 @@ import type { MetadataRoute } from 'next';
 
 export default function manifest(): MetadataRoute.Manifest {
     return {
-        name: 'Next.js PWA',
-        short_name: 'NextPWA',
+        name: 'My Web Push',
+        short_name: 'My Web Push',
         description: 'A Progressive Web App built with Next.js',
         start_url: '/',
         display: 'standalone',
