27457abd47142110b2cca6352fb563e7e9fa28ea
EchoLi <echoli@gouptech.com.tw>
2025-03-27 17:19:48 +0800

    expose NEXT_PUBLIC_FROG_TEST to client

--- app/vap.ts
+++ app/vap.ts
@@ -1,10 +1,10 @@
 export const subject = 'https://mywebpush.vercel.app/';
 export const publicKey =
-  process.env.FROG_TEST === '1'
+  process.env.NEXT_PUBLIC_FROG_TEST === '1'
     ? process.env.NEXT_PUBLIC_FROG_VAPID_PUBLIC_KEY!
     : process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY!;
 export const privateKey =
-  process.env.FROG_TEST === '1'
+  process.env.NEXT_PUBLIC_FROG_TEST === '1'
     ? process.env.FROG_VAPID_PRIVATE_KEY!
     : process.env.VAPID_PRIVATE_KEY!;
 
